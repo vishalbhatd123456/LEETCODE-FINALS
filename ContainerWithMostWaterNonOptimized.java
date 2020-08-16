9e6b726770da987217d84a387d6b6772e08edc92
VishalBhatD <60818841+vishalbhatd123456@users.noreply.github.com>
2020-08-16 22:26:41 +0530

    Create ContainerWithMostWaterNonOptimized.java

---
+++ ContainerWithMostWaterNonOptimized.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxArea(int[] height) {
+       //O(N2) 
+      int max = Integer.MIN_VALUE;
+        
+        for(int i=0;i<height.length;i++)
+        {
+            for(int j=i+1;j<height.length;j++)
+            {
+                int min = Math.min(height[i],height[j]);
+                max = Math.max(max,min*(j-i));
+            }
+        }
+        return max;
+}
+}
