eb8a73a39a50648662153e5cb64e87a872922205
VishalBhatD <60818841+vishalbhatd123456@users.noreply.github.com>
2020-08-16 22:38:57 +0530

    Create ContainerWithMostWaterOptimized.java

---
+++ ContainerWithMostWaterOptimized.java
@@ -0,0 +1,24 @@
+public int maxArea(int[] height)
+{
+int max = Integer.MIN_VALUE; //we start fromt he lowest value
+        
+        int i = 0;
+        int j = height.length-1;
+        
+        while(i<j)
+        {
+            int min = Math.min(height[i],height[j]);
+            max = Math.max(max,min*(j-i));
+            
+            if(height[i]<height[j])
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+        
+        return max;
+}
